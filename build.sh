6e8834b3268baffa450e4af79e1e080c38d7bc84
Lazar Obradovic <laz.obradovic@gmail.com>
2017-12-04 20:13:06 +0100

    Minor edits.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t mqtt-benchmark:latest .
+docker build --force-rm  -t mqtt-benchmark:latest .
--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ TARGETHOST=192.168.86.199
 
 if [ ! -d `pwd`/results ]; then
   echo No results directory here. Please create one - EXITING.
+  exit
 fi
 
-docker -it --rm -e TARGETHOST=$TARGETHOST -v `pwd`/results:/results mqtt-benchmark:latest
+docker run -it --rm -e TARGETHOST=$TARGETHOST -v `pwd`/results:/results mqtt-benchmark:latest
