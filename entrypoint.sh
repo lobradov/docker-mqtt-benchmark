e5020bad6653210fb4cfa9a8a2ae3a71303143f6
Lazar Obradovic <laz.obradovic@gmail.com>
2017-12-04 21:42:45 +0100

    ugly formatting fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,17 +13,17 @@ for clients in 1 2 4 8 16 32 64 128 256 512 1024 2048; do
   for size in 10 20 40 80 160 320 640; do
     echo -n "Testing with $clients clients and packet size of $size..."
     if [ -s /results/results-$clients-$size.json ]; then
-      echo "\t existing, SKIPPED"
+      echo "existing, SKIPPED"
       continue
     fi
     mqtt-benchmark -broker tcp://$TARGETHOST:1883 -clients $clients -size $size -count 5000 -quiet -format json > /results/results-$clients-$size.json
     if [ $? -ne 0 ]; then
       rm /results/results-$clients-$size.json
-      echo "\t FAILED, exiting!"
+      echo "FAILED, exiting!"
       exit
     fi
-    echo -n "\t finished!... sleeping 15s..."
+    echo -n "finished!... sleeping 15s..."
     sleep 15s
-    echo "\t DONE!"
+    echo "DONE!"
   done
 done
