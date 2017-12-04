3b053ce56eba5b852a0b41c462ba5e0f557ee513
Lazar Obradovic <laz.obradovic@gmail.com>
2017-12-04 20:23:32 +0100

    skip existing results.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,17 @@ echo "Starting benchmark"
 for clients in 1 2 4 8 16 32 64 128 256 512 1024 2048; do
   for size in 10 20 40 80 160 320 640; do
     echo -n "Testing with $clients clients and packet size of $size..."
+    if [ -s /results/mqtt-benchmark-results-$clients-$size.json ]; then
+      echo "\t existing, SKIPPED"
+      continue
+    fi
     mqtt-benchmark -broker tcp://$TARGETHOST:1883 -clients $clients -size $size -count 5000 -quiet -format json > /results/mqtt-benchmark-results-$clients-$size.json
     if [ $? -ne 0 ]; then
-      echo "FAILED, exiting!"
+      echo "\t FAILED, exiting!"
       exit
     fi
-    echo -n "finished!... sleeping 15s..."
+    echo -n "\t finished!... sleeping 15s..."
     sleep 15s
-    echo "DONE!"
+    echo "\t DONE!"
   done
 done
